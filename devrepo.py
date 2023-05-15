7589d7804be84e35d2c924a4979882075d48b428
B. Ranadeer <davidbasiripogu@hotmail.com>
2023-05-15 23:23:50 +0530

    used for loop to iterate

--- devrepo.py
+++ devrepo.py
@@ -1,8 +1,9 @@
 u = int(input("Enter Your Number :"))
 
-if u > 10:
-    print("Welcome Back!")
-elif u < 10:
-    print("Go Back")
-else:
-    print("Maybe think about yourself")
+for i in range(0,5):
+    if u > 10:
+        print("Welcome Back!")
+    elif u < 10:
+        print("Go Back")
+    else:
+        print("Maybe think about yourself")
